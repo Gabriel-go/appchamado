a521d4160ed0d2480a73dbe1c7de0f528fcd5d70
unknown <gabrielalexandregoias@gmail.com>
2021-11-05 00:10:08 -0300

    Implementado a pesquisa e o listview da tela de chamado

--- lib/chamado.dart
+++ lib/chamado.dart
@@ -9,13 +9,54 @@ class Chamado extends StatefulWidget {
 class _ChamadoState extends State<Chamado> {
   @override
   Widget build(BuildContext context) {
+    String src =
+        "https://www.iconpacks.net/icons/2/free-7-24-customer-support-icon-1719-thumb.png";
+    final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
     return Scaffold(
       appBar: AppBar(
         title: Text('Chamados'),
         backgroundColor: corPrincipal,
       ),
-      body: Column(
-        children: [Text("data")],
+      body: ListView(
+        children: [
+          Form(
+            key: _formKey,
+            child: Column(
+              children: [
+                TextFormField(
+                  autofocus: true,
+                  decoration: const InputDecoration(
+                    labelText: 'Pesquisa',
+                    labelStyle: TextStyle(
+                        color: Colors.black38,
+                        fontWeight: FontWeight.w400,
+                        fontSize: 20),
+                  ),
+                  validator: (String? value) {},
+                  onSaved: (vl) {},
+                  onChanged: (vl) {},
+                ),
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          for (int i = 0; i < 100; i++)
+            ListTile(
+              leading: Image(
+                image: NetworkImage(src),
+              ),
+              title: Text('ID: ${i} - Descrição do chamado'),
+              subtitle:
+                  Text('Uma breve descrição do que esta acontecendo la dentro'),
+              trailing: IconButton(
+                icon: Icon(Icons.edit),
+                color: Colors.blue,
+                onPressed: () {},
+              ),
+            ),
+        ],
       ),
     );
   }
